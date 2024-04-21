d7b427771bc39d171340683c5f72ce8c3a08567d
David Zeng <dzeng91@gmail.com>
2024-04-21 05:18:09 -0400

    draft1

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,14 @@ const Contact = () => {
     return (
         <div className="contact">
             <div className="email">Email: dzeng91@gmail.com</div>
-            <div className="github">Github: github.com/dzeng91</div>
+            <div className="github">
+                Github: <a href="https://github.com/dzeng91" target="_blank" rel="noopener noreferrer">github.com/dzeng91</a>
+            </div>
+            <div className="linkedin">
+                LinkedIn: <a href="https://www.linkedin.com/in/davidzeng-hello" target="_blank" rel="noopener noreferrer">www.linkedin.com/in/davidzeng-hello</a>
+            </div>
+
+            <div className="phone">Phone #: 917-302-0400</div>
         </div>
     );
 }
